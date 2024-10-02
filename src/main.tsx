10792bbd7a513788daad943fa05ed3735f23a51e
johnny.thai <johnny.thai@starixsoft.vn>
2024-10-02 17:40:17 +0700

    feat: layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./layout/Layout.tsx";
 
-createRoot(document.getElementById('__next')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+createRoot(document.getElementById("__next")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Layout>
+      <RouterProvider router={router} />
+    </Layout>
+  </StrictMode>
+);
