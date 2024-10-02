0001f48f492ae69f68e5d4956f4641669d7e3ffc
johnny.thai <johnny.thai@starixsoft.vn>
2024-10-02 17:47:24 +0700

    feat: speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import Layout from "./layout/Layout.tsx";
 
 const router = createBrowserRouter([
@@ -16,6 +17,7 @@ createRoot(document.getElementById("__next")!).render(
   <StrictMode>
     <Layout>
       <RouterProvider router={router} />
+      <SpeedInsights />
     </Layout>
   </StrictMode>
 );
